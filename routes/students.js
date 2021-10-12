f934200e4e87d3fbe80d95e24f52ec8d3ccebaeb
SamsAgency <samstore637@gmail.com>
2021-10-12 20:48:45 +0300

    added put & delete routes for student tuitors and users

--- routes/students.js
+++ routes/students.js
@@ -28,4 +28,39 @@ router.post("/", async (req, res) => {
 	.send(_.pick(student, ["_id", "firstname", "email"]));
 });
 
+// update
+router.put("/:id", async (req, res) => {
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	// find is there is an existing email
+	const email = await Tuitor.findOne({ email: req.body.email });
+	if (!email) return res.status(404).send("Invalid email");
+
+	const tuitor = await Tuitor.findByIdAndUpdate(
+	req.params.id,
+	{ $set: req.body },
+	{ new: true }
+	);
+
+	// hashing the passwords
+	const salt = await bcrypt.genSalt(10);
+	tuitor.password = await bcrypt.hash(tuitor.password, salt);
+
+	await tuitor.save();
+
+	const token = tuitor.generateAuthToken();
+	res
+	.header("x-auth-token", token)
+	.send(_.pick(tuitor, ["_id", "name", "email"]));
+});
+
+// delete
+router.delete("/:id", async (req, res) => {
+	const tuitor = await Tuitor.findByIdAndRemove(req.params.id);
+	if (!tuitor) return res.status(404).send("Tuitor does not exist");
+	res.send(tuitor);
+});
+
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,6 @@ router.post("/", async (req, res) => {
 	if (existingUser) return res.status(400).send("User already registered.");
 
 	let user = new User(req.body);
-	// user = new User(_.pick(req.body, ["name", "email", "password"]));
 	const salt = await bcrypt.genSalt(10);
 	user.password = await bcrypt.hash(user.password, salt);
 	await user.save();
@@ -34,6 +33,40 @@ router.post("/", async (req, res) => {
 	res
 	.header("x-auth-token", token)
 	.send(_.pick(user, ["_id", "name", "email"]));
-	});
+});
 
+	// update
+router.put("/:id", async (req, res) => {
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	// find is there is an existing email
+	const email = await User.findOne({ email: req.body.email });
+	if (!email) return res.status(404).send("Invalid email");
+
+	const user = await User.findByIdAndUpdate(
+	req.params.id,
+	{ $set: req.body },
+	{ new: true }
+	);
+
+	// hashing the passwords
+	const salt = await bcrypt.genSalt(10);
+	user.password = await bcrypt.hash(user.password, salt);
+
+	await user.save();
+
+	const token = user.generateAuthToken();
+	res
+	.header("x-auth-token", token)
+	.send(_.pick(user, ["_id", "name", "email"]));
+});
+
+// delete
+router.delete("/:id", async (req, res) => {
+	const user = await User.findByIdAndRemove(req.params.id);
+	if (!user) return res.status(404).send("User does not exist");
+	res.send(user);
+});
+	
 module.exports = router;
